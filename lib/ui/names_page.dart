35d5b56280b43939b96ca45271955d2ad5867acf
Stephan Veenstra <stephan.veenstra@pinch.nl>
2022-03-17 11:17:57 +0100

    Improve NamesPage with loading state

--- lib/ui/names_page.dart
+++ lib/ui/names_page.dart
@@ -4,11 +4,13 @@ class NamesPage extends StatelessWidget {
   final List<String> names;
   final void Function(BuildContext context)? onGenerateNamePressed;
   final void Function(String name, BuildContext context)? onRemoveNamePressed;
+  final bool isLoading;
 
   const NamesPage({
     required this.names,
     this.onGenerateNamePressed,
     this.onRemoveNamePressed,
+    this.isLoading = false,
     Key? key,
   }) : super(key: key);
 
@@ -18,23 +20,36 @@ class NamesPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Names'),
       ),
-      body: ListView.builder(
-        itemCount: names.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(names[index]),
-          trailing: onRemoveNamePressed == null
-              ? null
-              : IconButton(
-                  onPressed: () => onRemoveNamePressed!.call(names[index], context),
-                  icon: const Icon(Icons.delete),
-                ),
-        ),
+      body: Column(
+        children: [
+          if (isLoading)
+            const LinearProgressIndicator(
+              minHeight: 4,
+            ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: isLoading ? 0 : 4),
+              itemCount: names.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(names[index]),
+                trailing: onRemoveNamePressed == null
+                    ? null
+                    : IconButton(
+                        onPressed: () => onRemoveNamePressed!.call(names[index], context),
+                        icon: const Icon(Icons.delete),
+                      ),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: onGenerateNamePressed == null
           ? null
           : FloatingActionButton(
-              onPressed: () => onGenerateNamePressed!.call(context),
+              onPressed: isLoading ? null : () => onGenerateNamePressed!.call(context),
               child: const Icon(Icons.add),
+              disabledElevation: 0,
+              backgroundColor: isLoading ? Colors.blue.shade800 : null,
             ),
     );
   }
